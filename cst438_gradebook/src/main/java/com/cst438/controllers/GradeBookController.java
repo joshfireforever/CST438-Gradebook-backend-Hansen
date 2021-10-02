4f7aba341eb5ade94c33002e58692f1d62738fa6
joshfireforever <73496458+joshfireforever@users.noreply.github.com>
2021-10-02 00:10:28 -0700

    Update GradeBookController.java

--- cst438_gradebook/src/main/java/com/cst438/controllers/GradeBookController.java
+++ cst438_gradebook/src/main/java/com/cst438/controllers/GradeBookController.java
@@ -31,7 +31,7 @@ import com.cst438.domain.GradebookDTO;
 import com.cst438.services.RegistrationService;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = {"http://localhost:3000", "https://cst438-gradebook-fe-hansen.herokuapp.com/"})
 public class GradeBookController {
 	
 	@Autowired
